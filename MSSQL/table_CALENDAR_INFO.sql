2da90edebe399f08a17673e45d1f0c9cef935816
Oliver Guo <oliguo@users.noreply.github.com>
2021-12-30 11:40:56 +0800

    Create table_CALENDAR_INFO.sql

---
+++ MSSQL/table_CALENDAR_INFO.sql
@@ -0,0 +1,15 @@
+CREATE TABLE CALENDAR_INFO  
+(  
+COUNTRY nvarchar(2) not null,
+CDR_CODE nvarchar(4) not null,  
+DATE_NAME nvarchar(10) not null,  
+LUNAR nvarchar(30) not null,  
+YEAR nvarchar(10) not null,  
+MONTH nvarchar(10) not null,  
+WEEK nvarchar(10) not null,  
+DAY nvarchar(10) not null,  
+CDR_DATE nvarchar(20) not null,  
+CREATOR nvarchar(20) not null,  
+CREATE_DATE nvarchar(20) not null,  
+CREATE_INST nvarchar(20) not null  
+) 
